ff746364b634109be47a2a86db159679873091ae
Xiong Zhixuan <3536219460@qq.com>
2026-04-24 23:34:21 +0800

    Update param_node.py

--- my_first_package/my_first_package/param_node.py
+++ my_first_package/my_first_package/param_node.py
@@ -11,9 +11,8 @@ class MinimalParam(Node):
         # 创建定时器，每 1.0 秒调用一次下面的 self.timer_callback
         self.timer = self.create_timer(1.0, self.timer_callback)
 
-    # 就是这个函数！它不是文件，是类里的一个方法
     def timer_callback(self):
-        # 【重点】每次执行都要实时 get 一下参数值
+        # 每次执行都要实时 get 一下参数值
         my_param = self.get_parameter('my_config_param').get_parameter_value().string_value
         
         self.get_logger().info('Value: %s' % my_param)
